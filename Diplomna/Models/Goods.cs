57e408cd40aa303c996c45c66ff80522c31ffecc
Ivan Draganov <ivan.draganov@microinvest.net>
2018-10-31 11:22:02 +0200

    Second

---
+++ Diplomna/Models/Goods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplomna.Models
+{
+    class Goods
+    {
+        public string Name { get; set; }
+        public int PLU { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public int Qtty { get; set; }
+
+    }
+}
---
+++ Diplomna/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplomna.Models
+{
+    class Order
+    {
+        public int ID { get; set; }
+        public Dictionary<int, int> GoodsList { get; set; }
+        public ClientInfo Client { get; set; }
+
+    }
+}
