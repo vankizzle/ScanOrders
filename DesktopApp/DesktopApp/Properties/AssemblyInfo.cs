185960fd42f4d0eaaa35069d548e11c8d22de055
vankizzle <blaidzer@abv.bg>
2019-02-01 12:55:46 +0200

    Started the Desktop App

--- DesktopApp/DesktopApp/Properties/AssemblyInfo.cs
+++ DesktopApp/DesktopApp/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Diplomna")]
+[assembly: AssemblyTitle("DesktopApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Diplomna")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("DesktopApp")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- DesktopApp/DesktopApp/Properties/Resources.Designer.cs
+++ DesktopApp/DesktopApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Diplomna.Properties
+namespace DesktopApp.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace Diplomna.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Diplomna.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DesktopApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
